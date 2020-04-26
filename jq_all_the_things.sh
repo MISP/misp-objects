f12b2ecf29a0fe7e73299943e45d1195d928459a
Raphaël Vinot <raphael@vinot.info>
2020-04-26 03:02:02 +0200

    chg: Sort relationships

--- jq_all_the_things.sh
+++ jq_all_the_things.sh
@@ -22,7 +22,7 @@ do
     cat ${dir} | jq -S -j . | sponge ${dir}
 done
 
-cat relationships/definition.json | jq . | sponge relationships/definition.json
+cat relationships/definition.json | jq -S -j . | sponge relationships/definition.json
 
 cat schema_objects.json | jq . | sponge schema_objects.json
 cat schema_relationships.json | jq . | sponge schema_relationships.json
