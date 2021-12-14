ebbf1f13d6981bb0f96293e53aef8f823977b862
Alexandre Dulaunoy <a@foo.be>
2021-12-14 17:47:56 +0100

    new: [scripts] export added

---
+++ tools/updated.sh
@@ -0,0 +1,6 @@
+python3 adoc_objects.py >a.txt
+mv a.txt objects.txt
+asciidoctor-pdf  -a allow-uri-read  objects.txt 
+asciidoctor  -a allow-uri-read  objects.txt
+cp objects.html ../../misp-website/
+cp objects.pdf ../../misp-website/
